d79a62a692b7e42f410c867eaf95086348a76429
Globber11 <kg05052011@gmail.com>
2024-10-05 18:57:00 +0700

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from time import perf_counter
 def binary_search(data, elem):
     low = 0
     high = len(data) - 1
@@ -16,22 +16,32 @@ def binary_search(data, elem):
             low = middle + 1
     return -1
 
-numbers = [randint(0, 9) for _ in range(10)]
-numbers = set(numbers)
-numbers = list(numbers)
-numbers.sort()
-print(numbers)
+while True:
+    numbers = [randint(0, 9) for _ in range(10)]
+    numbers = set(numbers)
+    numbers = list(numbers)
+    numbers.sort()
+    print(numbers)
 
-random_number = randint(0, len(numbers))
-print(random_number)
+    random_number = randint(0, len(numbers) - 1)
+    print(random_number)
 
-try:
-    try_ = int(input("Попробуйте угадать - какое из этих чисел я загадал? :"))
+    try:
+        try_ = int(input("Попробуйте угадать - какое из этих чисел я загадал? :"))
+        tic = perf_counter()
 
-    if binary_search(numbers, try_) == random_number:
-        print("Харош, угадал")
-    else:
-        print("Не угадал, лошара")
+        if binary_search(numbers, try_) == random_number:
+            toc = perf_counter()
+            print(f"Харош, угадал за {toc - tic:0.4f} секунд")
 
-except:
-    print("Нормально введи, олень")
+        else:
+            print("Не угадал, лошара")
+
+    except:
+        print("Нормально введи, олень")
+    next_ = input("Продолжить?")
+    if next_ == "Нет":
+        break
+    elif next_ == "Да":
+        continue
+print("Работа программы завершена")
